0f13d4cbe10138c37ec4eded12c01ebf9ded5947
Christian Posta <christian.posta@gmail.com>
2017-10-05 09:27:18 -0700

    added embedded arq testing

--- backend-v2/src/main/java/org/jboss/examples/ticketmonster/servlet/FF4jConsoleServlet.java
+++ backend-v2/src/main/java/org/jboss/examples/ticketmonster/servlet/FF4jConsoleServlet.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.examples.ticketmonster.util;
+package org.jboss.examples.ticketmonster.servlet;
 
 import org.ff4j.web.embedded.ConsoleServlet;
 
--- backend-v2/src/test/java/org/jboss/examples/ticketmonster/test/TicketMonsterDeployment.java
+++ backend-v2/src/test/java/org/jboss/examples/ticketmonster/test/TicketMonsterDeployment.java
@@ -4,10 +4,16 @@ import org.jboss.examples.ticketmonster.util.Resources;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+
+import java.io.File;
 
 public class TicketMonsterDeployment {
 
     public static WebArchive deployment() {
+
+        File[] ff4jDeps = Maven.resolver().resolve("org.ff4j:ff4j-core:1.6.5")
+                .withTransitivity().asFile();
         return ShrinkWrap
                 .create(WebArchive.class, "test.war")
                 .addPackage(Resources.class.getPackage())
@@ -15,6 +21,9 @@ public class TicketMonsterDeployment {
                 .addAsResource("import.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
                 // Deploy our test datasource
-                .addAsWebInfResource("test-ds.xml");
+                .addAsWebInfResource("test-ds.xml")
+                .addAsLibraries(ff4jDeps)
+                .addAsResource("ff4j.xml")
+                ;
     }
 }
