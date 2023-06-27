a8fdd1d97582fccb6e411006f1885512c20e5638
Eoan O Dea <odeaeoan@gmail.com>
2023-06-27 16:05:53 +0200

    Improving monitor.sh script

--- monitor.sh
+++ monitor.sh
@@ -43,51 +43,35 @@ for container in "${containers[@]}"; do
   done
 done
 
-echo "All containers are running. Waiting for 5 seconds before starting monitoring..."
+echo "All containers are running. Monitoring starts in 5 seconds..."
 sleep 5
 
 echo "Monitoring CPU usage of Docker containers for $duration seconds..."
 
-# Function to prettify the output table
-prettify_table() {
-  awk 'BEGIN { FS="\t"; OFS="\t" } { $1=sprintf("%-30s", $1); $2=sprintf("%-10s", $2); print }'
-}
 
-# Store the start time
-start_time=$(date +%s)
-
-# Array to store CPU usage values for each container
 cpu_usages=()
 
-echo -e "\n"
-
-# Start monitoring CPU usage for each container
+# Loop through the container names and track CPU usage
 for container in "${containers[@]}"; do
-
-  (docker stats --format "{{.Container}}: {{.CPUPerc}}" --no-stream "$container" | \
-    while read -r line; do
-      # Extract the CPU usage value from the line
-      cpu_usage=$(echo "$line" | awk '{gsub("%",""); print $NF}')
-
-      # Clear the last line, move to the beginning of the line, insert a line break, and display updated CPU usage
-    tput cuu 1 && tput el && echo -e "Container: $container - CPU usage: $cpu_usage%"
-    done) &
-done
-
-# Wait for the specified duration
-sleep "$duration"
-
-# Terminate the background processes
-pkill -P $$ docker
-
-# Calculate the energy consumption in kilowatt-hours (kWh)
-total_power_consumption=0
-for container in "${containers[@]}"; do
+  echo "Monitoring CPU usage for container: $container"
   cpu_usage=$(docker stats --format "{{.CPUPerc}}" --no-stream "$container" | awk '{gsub("%",""); sum += $1} END {print sum}')
-  power_usage=$(echo "$cpu_usage * $power_consumption * $duration / 100" | bc)
-  total_power_consumption=$(echo "$total_power_consumption + $power_usage" | bc)
+  cpu_usages+=("$cpu_usage")
 done
 
-energy_consumption=$(echo "($total_power_consumption + $power_r)" | bc)
+# Calculate the average CPU usage
+num_containers=${#containers[@]}
+total_cpu_usage=0
 
+for usage in "${cpu_usages[@]}"; do
+  total_cpu_usage=$(echo "$total_cpu_usage + $usage" | bc)
+done
+
+
+average_cpu_usage=$(echo "scale=2; $total_cpu_usage / $num_containers" | bc)
+
+# Calculate the energy consumption in watt-hours (Wh)
+power_consumption=100  # Power consumption of the containers in watts
+energy_consumption=$(echo "scale=2; $average_cpu_usage * $power_consumption * $duration / 3600" | bc)
+
+echo "Average CPU usage: $average_cpu_usage%"
 echo "Energy consumption during $duration seconds: $energy_consumption Wh"
