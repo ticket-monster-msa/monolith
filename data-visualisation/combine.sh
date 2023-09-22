9e5d450f4a3fd154e92a21b471064cfdd650707a
Eoan O Dea <odeaeoan@gmail.com>
2023-09-22 11:10:29 +0200

    Updated combine.sh script to include energy metrics in Joules, and temperature of machine

--- data-visualisation/combine.sh
+++ data-visualisation/combine.sh
@@ -4,7 +4,7 @@
 output_file="extracted_values.csv"
 
 # Create the header in the output file
-echo "#,DateTime,Architecture,File,Total Elapsed Time (sec),Cumulative Package Energy_0 (mWh),Cumulative IA Energy_0 (mWh),Cumulative DRAM Energy_0 (mWh)" > "$output_file"
+echo "#,DateTime,Architecture,File,Total Elapsed Time (sec),Cumulative Package Energy_0 (mWh),Cumulative IA Energy_0 (mWh),Cumulative DRAM Energy_0 (mWh),Cumulative Package Energy_0 (Joules),Cumulative IA Energy_0 (Joules),Cumulative DRAM Energy_0 (Joules), Package Temperature_0 (C)" > "$output_file"
 
 # Function to extract float values from a file
 extract_values() {
@@ -15,14 +15,20 @@ extract_values() {
     
     # Initialize variables for values
     local elapsed_time=""
-    local package_energy=""
-    local ia_energy=""
-    local dram_energy=""
+    local package_energy_mwh=""
+    local ia_energy_mwh=""
+    local dram_energy_mwh=""
+    local package_energy_joules=""
+    local ia_energy_joules=""
+    local dram_energy_joules=""
     
     # echo "Extracting values from: $folder/$subfolder/$filename" # Debugging line
 
     # Ensure consistent line endings with dos2unix
     dos2unix -q "$file"
+
+    # Flag to indicate whether to start collecting Package Temperature values
+    local collect_temperature_values=false
     
     # Loop through lines in the file
     while IFS= read -r line; do
@@ -30,19 +36,51 @@ extract_values() {
         if [[ $line =~ "Total Elapsed Time (sec) = " ]]; then
             elapsed_time=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
         elif [[ $line =~ "Cumulative Package Energy_0 (mWh) = " ]]; then
-            package_energy=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
+            package_energy_mwh=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
+        elif [[ $line =~ "Cumulative Package Energy_0 (Joules) = " ]]; then
+            package_energy_joules=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
         elif [[ $line =~ "Cumulative IA Energy_0 (mWh) = " ]]; then
-            ia_energy=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
+            ia_energy_mwh=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
+        elif [[ $line =~ "Cumulative IA Energy_0 (Joules) = " ]]; then
+            ia_energy_joules=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
         elif [[ $line =~ "Cumulative DRAM Energy_0 (mWh) = " ]]; then
-            dram_energy=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
-        fi
+            dram_energy_mwh=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
+        elif [[ $line =~ "Cumulative DRAM Energy_0 (Joules) = " ]]; then
+            dram_energy_joules=$(echo "$line" | awk -F ' = ' '{print $2}' | tr -d '"')
+    fi
     done < "$file"
+
+
+python_script=$(cat <<END
+import pandas as pd
+
+# Input CSV file
+input_file = "$file"
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(input_file)
+
+# Extract values under the "Package Temperature_0(C)" header into a list
+package_temperature_values = df["Package Temperature_0(C)"].dropna().tolist()
+
+# Calculate the average of the collected values
+average_temperature = sum(package_temperature_values) / len(package_temperature_values)
+
+# Print the average temperature
+print(average_temperature)
+END
+)
+
+
+    # Execute the Python script and capture the output as a Bash variable
+    average_temperature=$(python -c "$python_script")
     
     # Debugging lines to print extracted values
     echo "Elapsed Time: $elapsed_time"
     echo "Package Energy: $package_energy"
     echo "IA Energy: $ia_energy"
     echo "DRAM Energy: $dram_energy"
+    echo "Average Package Temperature (C): $average_temperature"
 
     # Split the subfolder based on the / character into two variables
     IFS='/' read -r subfolder1 subfolder2 <<< "$subfolder"
@@ -52,7 +90,7 @@ extract_values() {
     name_without_extension=$(echo "$name_with_extension" | cut -d'.' -f1)
 
     # Append the extracted values to the output file, removing any unwanted characters
-    echo "$number,$subfolder1,$subfolder2,$name_without_extension,$elapsed_time,$package_energy,$ia_energy,$dram_energy" >> "$output_file"
+    echo "$number,$subfolder1,$subfolder2,$name_without_extension,$elapsed_time,$package_energy_mwh,$ia_energy_mwh,$dram_energy_mwh,$package_energy_joules,$ia_energy_joules,$dram_energy_joules,$average_temperature" >> "$output_file"
 }
 
 # Loop through the outer folders (e.g., "19-09-23T19-32-10", "20-09-23T02-30-52")
