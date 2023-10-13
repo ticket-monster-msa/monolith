76ddcf8a1ba200691e63547001aae65684d8bbd9
Eoan O Dea <odeaeoan@gmail.com>
2023-10-13 10:47:12 +0200

    Improved prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -7,40 +7,34 @@ echo "---------------------------------------------"
 echo "Checking prerequisites for experiment"
 echo "---------------------------------------------"
 
-# Check if Node.js is installed
-if ! command -v node &> /dev/null; then
-    echo "Node.js is not installed. Please install Node.js."
-    exit 1
-fi
 
-# Check if npm (Node Package Manager) is installed
-if ! command -v npm &> /dev/null; then
-    echo "npm (Node Package Manager) is not installed. Please install npm."
-    exit 1
-fi
+# Array of commands/applications to check
+commands_to_check=(
+    "/Applications/Intel Power Gadget/PowerLog:PowerLog application"
+    "node:Node.js"
+    "npm:npm (Node Package Manager)"
+    "newman:Newman (Postman command-line tool)"
+    "python:Python"
+    "docker:Docker"
+)
 
-# Check if Newman (Postman command-line tool) is installed
-if ! command -v newman &> /dev/null; then
-    echo "Newman is not installed. Please install Newman (Postman command-line tool)."
-    exit 1
-fi
 
-# Check if Python is installed
-if ! command -v python &> /dev/null; then
-    echo "Python is not installed. Please install Python."
-    exit 1
-fi
+for command_entry in "${commands_to_check[@]}"; do
+    IFS=":" read -r command_name display_name <<< "$command_entry"
 
-# Check if Docker is installed and running
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Please install Docker."
-    exit 1
-fi
+    if ! command -v "$command_name" &> /dev/null; then
+        echo "$display_name is not installed. Please install $display_name."
+        exit 1
+    fi
+    echo "- $display_name is installed."
+done
 
 if ! docker info &> /dev/null; then
     echo "Docker is not running. Please start Docker."
     exit 1
 fi
+echo "- Docker is running."
+
 
 # Check if the specified files exist
 files_to_check=(
@@ -56,6 +50,7 @@ for file in "${files_to_check[@]}"; do
         echo "File $file does not exist."
         exit 1
     fi
+    echo "- File $file exists."
 done
 
 echo "---------------------------------------------"
